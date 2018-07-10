283ff751a62de6077b8ce41eac0495b6ca39de46
Hemant Kumar Sah <hemantanshu@gmail.com>
2018-07-10 12:26:06 +0530

    Added support for form type in custom forms

--- src/Models/CustomForm.php
+++ src/Models/CustomForm.php
@@ -2,8 +2,10 @@
 
 namespace Drivezy\LaravelRecordManager\Models;
 
+use Drivezy\LaravelAccessManager\Models\Route;
 use Drivezy\LaravelRecordManager\Observers\CustomFormObserver;
 use Drivezy\LaravelUtility\Models\BaseModel;
+use Drivezy\LaravelUtility\Models\LookupValue;
 
 /**
  * Class CustomForm
@@ -14,7 +16,31 @@ class CustomForm extends BaseModel {
      * @var string
      */
     protected $table = 'dz_custom_forms';
+    /**
+     * @var array
+     */
     protected $hidden = ['created_by', 'updated_by', 'created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function route () {
+        return $this->belongsTo(Route::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function method () {
+        return $this->belongsTo(LookupValue::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function form_type () {
+        return $this->belongsTo(LookupValue::class);
+    }
 
     /**
      * Override the boot functionality to add up the observer
