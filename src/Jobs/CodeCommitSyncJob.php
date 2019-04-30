c0d7716638ac851e0ddadf6aac61e7a4ca91592d
Hemant Kumar Sah <hemantanshu@gmail.com>
2019-05-01 00:13:33 +0530

    added support for syncing of code across servers on code commit

---
+++ src/Jobs/CodeCommitSyncJob.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drivezy\LaravelRecordManager\Jobs;
+
+use Drivezy\LaravelRecordManager\Models\CodeCommit;
+use Drivezy\LaravelUtility\Job\BaseJob;
+use Drivezy\LaravelUtility\Library\RemoteRequest;
+use Illuminate\Support\Facades\Crypt;
+
+/**
+ * Class CodeCommitSyncJob
+ * @package Drivezy\LaravelRecordManager\Jobs
+ */
+class CodeCommitSyncJob extends BaseJob {
+    /**
+     * @return bool
+     */
+    public function handle () {
+        parent::handle();
+
+        $commit = CodeCommit::find($this->id);
+        $servers = $this->getActiveServers($commit);
+
+        //if no servers are deployed against the deployment, then do nothing
+        if ( !sizeof($servers) ) return false;
+
+        $url = Utility::getProperty('deployment.server.url');
+        $token = Utility::getProperty('deployment.server.access.key');
+
+        //if properties are not setup do nothing
+        if ( !( $url && $token ) ) return false;
+
+        $token = Crypt::decrypt($token);
+
+        //create the payload for the request
+        $payload = [
+            'id'                => $commit->id,
+            'deployment_script' => 'custom-sync-all.sh',
+            'branch'            => $commit->branch,
+            'repository'        => $commit->repository_name,
+            'instances'         => [],
+        ];
+
+        foreach ( $servers as $server ) {
+            array_push($payload['instances'], [
+                'instance_id' => $server->instance_identifier,
+            ]);
+        }
+
+        RemoteRequest::pushJsonRequest($url . '/deploy', $payload, 'POST', ['access-key' => $token]);
+    }
+
+    /**
+     * @param CodeCommit $commit
+     * @return mixed
+     */
+    private function getActiveServers (CodeCommit $commit) {
+        return ServerDeployment::where('repository_name', $commit->repository_name)
+            ->where('branch', $commit->branch)
+            ->where('last_ping_time', '>', DateUtil::getPastTime(2))
+            ->whereNotNull('instance_identifier')
+            ->get();
+    }
+}
